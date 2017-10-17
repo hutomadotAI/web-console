e897f5b97c9851f489db2cf6af50427a86f06ed9
bretcolloff <bret.colloff@gmail.com>
2017-10-17 17:49:35 +0100

    Services and starting templates.

---
+++ src/entities/services.py
@@ -0,0 +1,56 @@
+import logging
+import requests
+import urllib
+
+from django.contrib.auth.models import AnonymousUser, User
+
+from django.conf import settings
+
+from app.services import set_headers
+
+logger = logging.getLogger(__name__)
+
+
+def get_entities(token=False):
+    """
+    Returns a list of the users entities.
+    """
+
+    path = '/entities'
+    url = settings.API_URL + path
+
+    logger.debug(url)
+
+    responseJSON = requests.get(
+        url,
+        headers=set_headers(token),
+        timeout=settings.API_TIMEOUT
+    ).json()
+
+    entities = responseJSON['entities']
+
+    logger.debug(entities)
+
+    return entities
+
+def get_entity(entityName, token=False):
+    """
+    Get a specific entity.
+    :param token: The user token.
+    :return: The entity JSON.
+    """
+
+    path = '/entity?entity_name={0}'
+    url = settings.API_URL + path.format(entityName)
+
+    logger.debug(url)
+
+    entity = requests.get(
+        url,
+        headers=set_headers(token),
+        timeout=settings.API_TIMEOUT
+    ).json()
+
+    logger.debug(entity)
+
+    return entity
---
+++ src/entities/views.py
@@ -0,0 +1,28 @@
+import logging
+import urllib
+
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+
+from services import get_entities, get_entity
+
+logger = logging.getLogger(__name__)
+
+
+class EntityListView(ListView):
+    """
+    List view of bots, filterable by category
+    """
+    context_object_name = 'entities'
+    template_name = 'entity.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(EntityListView, self).get_context_data(**kwargs)
+        context['category'] = urllib.parse.unquote(self.kwargs['category'])
+        context['token'] = self.request.session.get('token', False)
+
+        return context
+
+    def get_queryset(self):
+        return get_entities(token=self.request.session.get('token', False))
+
