808c24cf9fa63b6daa5a43891b17b95094ef4772
Karol Jastrzębowski <karol@hutoma.ai>
2017-12-12 15:35:25 +0000

    Merged PR 2098: Fix encoding while creating new user in API #3860
    Fix encoding while creating new user in API #3860
    
    Related work items: #3860

--- src/users/services.py
+++ src/users/services.py
@@ -1,6 +1,8 @@
 import logging
 import requests
 
+import urllib.parse # used for encoding new user creation
+
 from django.conf import settings
 
 from app.services import set_headers
@@ -35,10 +37,10 @@ def post_user(api_admin_token, user_data):
 
     path = '/admin/?email={0}&username={1}&first_name={2}&last_name={3}'
     url = settings.API_URL + path.format(
-        user_data.email,
-        user_data.username,
-        user_data.first_name,
-        user_data.last_name
+        urllib.parse.quote_plus(user_data.email),
+        urllib.parse.quote_plus(user_data.username),
+        urllib.parse.quote_plus(user_data.first_name),
+        urllib.parse.quote_plus(user_data.last_name)
     )
 
     logger.debug([url, api_admin_token])
