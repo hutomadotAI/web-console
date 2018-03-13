77cc9cd27abdc960550f8a2bff129d8b1d2ad3e0
Karol Jastrzębowski <karol@hutoma.ai>
2018-03-13 15:51:32 +0000

    Merged PR 2418: Add change events to Enhanced textarea #4212
    Add change events to Enhanced textarea #4212
    
    Related work items: #4212

--- src/studio/static/js/enhanced_textarea.js
+++ src/studio/static/js/enhanced_textarea.js
@@ -1,27 +1,52 @@
-const TEXTAREA = document.getElementById('id_training_data');
-const FILE = document.getElementById('FILE');
+(function() {
+  const TEXTAREA = document.getElementById('id_training_data');
+  const FILE = document.getElementById('FILE');
 
-function loadHandler(event) {
-  TEXTAREA.classList.remove('loading');
-  TEXTAREA.value += this.result;
-}
-
-function fileHandler(event) {
-  event.preventDefault();
-
-  TEXTAREA.value = '';
-
-  let files = (event.dataTransfer || event.target).files;
-
-  TEXTAREA.classList.add('loading');
-
-  for (file of files) {
-    let reader = new FileReader();
-    reader.addEventListener('load', loadHandler);
-    reader.readAsText(file);
+  /**
+   * Updates textarea and removes loading class
+   *
+   * @param  {Event object} event
+   */
+  function loadHandler(event) {
+    TEXTAREA.classList.remove('loading');
+    TEXTAREA.value += this.result;
   }
 
-}
+  /**
+   * Clears textarea, adds loading class and proceeds uploaded or dropped files.
+   *
+   * @param  {Event object} event
+   */
+  function fileHandler(event) {
+    event.preventDefault();
 
-TEXTAREA.addEventListener('drop', fileHandler);
-FILE.addEventListener('change', fileHandler);
+    TEXTAREA.value = '';
+
+    let files = (event.dataTransfer || event.target).files;
+
+    TEXTAREA.classList.add('loading');
+
+    for (file of files) {
+      let reader = new FileReader();
+      reader.addEventListener('load', loadHandler);
+      reader.readAsText(file);
+    }
+
+  }
+
+  /**
+   * Triggers change event
+   *
+   * @param  {Event object} event
+   */
+  function syntheticChange(event) {
+    TEXTAREA.dispatchEvent(new Event('change', { 'bubbles': true }))
+  }
+
+  TEXTAREA.addEventListener('drop', fileHandler);
+  TEXTAREA.addEventListener('paste', syntheticChange);
+  TEXTAREA.addEventListener('keyup', syntheticChange);
+  TEXTAREA.addEventListener('drop', syntheticChange);
+  FILE.addEventListener('change', syntheticChange);
+  FILE.addEventListener('change', fileHandler);
+})();
