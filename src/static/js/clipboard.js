823c5e50089ec14fa50770e7921b553e9bdaeb88
Karol Jastrzębowski <karol@hutoma.ai>
2018-05-09 09:35:49 +0000

    Merged PR 2742: Update dependencies versions
    Update dependencies versions
    
    Python container:
      * python:3.6.4-slim-jessie > python:3.6.5-slim-jessie
    
    Dependencies python:
      * coloredlogs 9.0 > 9.3.1
      * gunicorn 19.7.1 > 19.8.1
      * django 2.0.2 > 2.0.5
      * django-allauth 0.35.0 > 0.36.0
      * django-constance 2.1.0 > 2.2.0
      * django-crispy-forms 1.7.0 > 1.7.2
      * django-countries 5.1.1 > 5.3
      * django-redis 4.8.0 > 4.9.0
      * Pillow 5.0.0 > 5.1.0
      * django-widget-tweaks 1.4.1 > 1.4.2
      * factory_boy 2.10.0 > 2.11.1
    
    Dependencies js/css:
      * twitter-bootstrap 4.0.0 > 4.1.1
      * popper.js 1.14.1 > 1.14.3
      * bootstrap-datepicker 1.7.1 > 1.8.0
      * prism 1.13.0 > 1.14.0
      * js-cookie 2.1.4 > 2.2.0
      * clipboard.js 1.7.1 > 2.0.1
      * cookie-banner 1.2.1 > 1.2.2
      * normalize 7.0.0 > 8.0.0
    
    Related work items: #4905

--- src/static/js/clipboard.js
+++ src/static/js/clipboard.js
@@ -1,24 +1,24 @@
-document.addEventListener('DOMContentLoaded', function(event) {
+document.addEventListener('DOMContentLoaded', function() {
   // Enable copy to clipboard for all the triggerers containing targets
-  const clipboard = new Clipboard('[data-clipboard-target]')
+  const clipboard = new ClipboardJS('[data-clipboard-target]');
 
   function setTooltip(message, trigger) {
     $(trigger)
       .attr('data-original-title', message)
-      .tooltip('show')
+      .tooltip('show');
   }
 
   function hideTooltip(trigger) {
     setTimeout(function() {
       $(trigger)
         .tooltip('hide')
-        .attr('data-original-title', 'Copy to clipboard')
-    }, 2000)
+        .attr('data-original-title', 'Copy to clipboard');
+    }, 2000);
   }
 
   clipboard.on('success', function(event) {
-    setTooltip('Copied!', event.trigger)
-    hideTooltip(event.trigger)
-  })
+    setTooltip('Copied!', event.trigger);
+    hideTooltip(event.trigger);
+  });
 
 });
