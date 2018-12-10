cfaf40b3273249fa176372cb547210818a0448b1
Karol Jastrzębowski <karol@hutoma.ai>
2018-12-10 13:24:31 +0000

    Merged PR 3736: Provide a specific message if 500 is caused by API timeout
    Provide a specific message if 500 is caused by API timeout
    
    Added: Pass error type to template context and trigger a different
    			 message base on it. #6589
    
    Related work items: #6589

--- src/app/errors.py
+++ src/app/errors.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import sys
 
 
 def handler400(request, exception):
@@ -18,4 +19,12 @@ def handler404(request, exception):
 
 
 def handler500(request):
-    return render(request, '500.html', locals(), status=500)
+    # We get more informations about the error from system info and we pass type
+    # to the template
+    type, value, traceback = sys.exc_info()
+    context = {
+        'request': request,
+        'error_type': type.__name__
+    }
+
+    return render(request, '500.html', context, status=500)
